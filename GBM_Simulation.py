d5bc2c01ca34701664cd082abac3b5a1a6c59635
Cameron Cooke <came.cooke@gmail.com>
2026-05-18 14:32:34 -0400

    Description of program updated

---
+++ GBM_Simulation.py
@@ -0,0 +1,9 @@
+# Cameron Cooke
+# Copyright © 2026
+
+"""
+This program utilizes Geometric Brownian Motion (GBM) to generate possible price
+paths for a selected equity. Using Monte Carlo simulation, the program generates
+numerous potential price paths by applying GBM calculation a specified number of
+iterations. The results are then displayed in a graphical format.
+"""
